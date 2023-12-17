850a2fc60f7f5621692aa2056895cfe80d17e6c0
scoxfield <alexandru.negru03@e-uvt.ro>
2023-12-17 02:52:58 +0200

    Worked on the App 'Must-Haves'

--- screens/HackatonsScreen.js
+++ screens/HackatonsScreen.js
@@ -1,16 +1,19 @@
-import React, {useState, useEffect} from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-import firebase from 'firebase/app';
-import {auth, database} from '../firebaseConfig';
-import {getDatabase, ref, onValue} from '@firebase/database';
+import React, { useState, useEffect } from 'react';
+import { ScrollView, View, Text, StyleSheet, Dimensions, TouchableOpacity } from 'react-native';
+import { getDatabase, ref, onValue } from '@firebase/database';
+import { LinearGradient } from 'expo-linear-gradient';
+
+const WhiteLine = () => <View style={styles.whiteLine}></View>;
 
 const HackatonsScreen = () => {
     const [hackathons, setHackathons] = useState([]);
     const database = getDatabase();
-    const hackathonsRef = ref(database, 'hackathons/')
-    console.log("database is:" + database);
+    const hackathonsRef = ref(database, 'hackathons/');
+
     useEffect(() => {
-        fetch('https://hackmate-1e8a7-default-rtdb.europe-west1.firebasedatabase.app/hackathons.json')
+        fetch(
+            'https://hackmate-1e8a7-default-rtdb.europe-west1.firebasedatabase.app/hackathons.json'
+        )
             .then((response) => response.json())
             .then((data) => {
                 if (data) {
@@ -22,19 +25,46 @@ const HackatonsScreen = () => {
                 console.error('Error fetching hackathons:', error);
             });
     }, []);
-    console.log(hackathons);
+
+    const screenWidth = Dimensions.get('window').width;
+
     return (
-        <View style={styles.container}>
-            <Text>Welcome to the Hackathons Screen!</Text>
-            {hackathons.map((hackathon, index) => (
-                <View key={index}>
-                    <Text>Name: {hackathon.name}</Text>
-                    <Text>Date: {hackathon.date}</Text>
-                    <Text>Duration: {hackathon.duration}</Text>
-                    {/* Display other hackathon details */}
+        <LinearGradient colors={['orange', 'red']} style={styles.container}>
+            <View>
+                <Text style={styles.headerText}>Choose Hackatons!</Text>
+            </View>
+            <ScrollView contentContainerStyle={styles.scrollContainer}>
+                <View style={styles.hackathonsContainer}>
+                    {hackathons.map((hackathon, index) => (
+                        <View
+                            key={index}
+                            style={[
+                                styles.hackathonEntry,
+                                {
+                                    flexDirection: 'row',
+                                    justifyContent: 'space-between',
+                                    width: screenWidth > 600 ? '45%' : '90%',
+                                },
+                            ]}
+                        >
+                            <View style={{ marginRight: 10 }}>
+                                <Text style={styles.hackathonName}>Name: {hackathon.name}</Text>
+                                <Text style={styles.date}>Date: {hackathon.date}</Text>
+                                <Text>Duration: {hackathon.duration}</Text>
+                                <Text>Finishdate: {hackathon.finishdate}</Text>
+                                <Text>Teamlimit: {hackathon.teamlimit}</Text>
+                            </View>
+                            <TouchableOpacity onPress={() => console.log('Button Pressed')}>
+                                <View style={styles.button}>
+                                    <Text style={styles.buttonText}>Attend!</Text>
+                                </View>
+                            </TouchableOpacity>
+                        </View>
+                    ))}
+                    <WhiteLine />
                 </View>
-            ))}
-        </View>
+            </ScrollView>
+        </LinearGradient>
     );
 };
 
@@ -43,7 +73,69 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
-        backgroundColor: 'lightgreen',
+    },
+    scrollContainer: {
+        flexGrow: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    headerText: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        color: 'white',
+        marginBottom: 20,
+        marginTop: 40,
+        padding: 5,
+    },
+    hackathonsContainer: {
+        width: '100%',
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        justifyContent: 'center',
+        paddingVertical: 20,
+    },
+    hackathonEntry: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: 'white',
+        borderRadius: 50,
+        elevation: 10,
+        shadowColor: 'black',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.3,
+        shadowRadius: 5,
+        padding: 20,
+        marginBottom: 10,
+    },
+    hackathonName: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        color: 'black',
+        marginBottom: 5,
+    },
+    whiteLine: {
+        backgroundColor: 'white',
+        height: 2,
+        width: '100%',
+        marginVertical: 10,
+    },
+    date: {
+        color: 'black',
+    },
+    button: {
+        right:25,
+        backgroundColor: 'orange',
+        padding: 15,
+        borderRadius: 20,
+        elevation: 10,
+        shadowColor: 'black',
+        shadowOffset: { width: 0, height: 1 },
+        shadowOpacity: 0.2,
+        shadowRadius: 3,
+    },
+    buttonText: {
+        color: 'white',
+        fontWeight: 'bold',
     },
 });
 
--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -1,29 +1,54 @@
-import React, { useContext } from 'react';
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, StyleSheet, TouchableOpacity, Image,  ActivityIndicator } from 'react-native';
 import { auth } from '../firebaseConfig';
 import { CommonActions } from '@react-navigation/native';
+import * as ImagePicker from 'expo-image-picker';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import firebase from 'firebase/app';
+import 'firebase/storage';
+import { getStorage, ref, uploadBytesResumable, getDownloadURL } from 'firebase/storage';
+import {sendPasswordResetEmail} from "firebase/auth";
 
 const WhiteLine = () => (
     <View style={styles.whiteLine}></View>
 );
 
 const ProfileScreen = ({ navigation }) => {
+    const [profileImage, setProfileImage] = useState(null);
+    const [uploading, setUploading] = useState(false);
+
+    // Load the stored image URI when the component mounts
+    useEffect(() => {
+        const loadProfileImage = async () => {
+            try {
+                const storage = getStorage();
+                const storageRef = ref(storage, `ProfilePictures/${auth.currentUser.uid}`);
+                const url = await getDownloadURL(storageRef);
+                console.log("Download URL:", url);
+                setProfileImage(url);
+            } catch (error) {
+                console.error('Error loading profile image:', error);
+            }
+        };
+
+        loadProfileImage();
+    }, [profileImage]);
+
     const handleResetPassword = () => {
         const email = auth.currentUser.email;
-
         // Check if the user has provided an email
         if (email) {
             // Send a password reset email
-            auth
-                .sendPasswordResetEmail(email)
+            sendPasswordResetEmail(auth, email)
                 .then(() => {
-                    console.log('Password reset email sent');
+                    alert('Check your email!');
                 })
                 .catch((error) => {
                     console.log(error.message);
                 });
         }
     };
+
 
     const handleLogOut = () => {
         auth.signOut().then(() => {
@@ -37,8 +62,82 @@ const ProfileScreen = ({ navigation }) => {
         });
     };
 
+    const handleProfilePictureUpload = async () => {
+        let result = await ImagePicker.launchImageLibraryAsync({
+            mediaTypes: ImagePicker.MediaTypeOptions.Images,
+            allowsEditing: true,
+            aspect: [4, 3],
+            quality: 1,
+        });
+
+        console.log(result);
+
+        if (!result.cancelled) {
+            setProfileImage(result.uri);
+            // Save the image URI locally
+            await AsyncStorage.setItem('profileImage', result.uri);
+            console.log("XDBUNA TU!");
+            await uploadProfilePicture(result.uri);
+            console.log("XBUNA TU!");
+        }
+    };
+
+    const uploadProfilePicture = async (imageUri) => {
+        const blob = await new Promise((resolve, reject) => {
+            const xhr = new XMLHttpRequest();
+            xhr.onload = function () {
+                resolve(xhr.response);
+            };
+            xhr.onerror = function () {
+                reject(new TypeError('Network request failed'));
+            };
+            xhr.responseType = 'blob';
+            xhr.open('GET', imageUri, true);
+            xhr.send(null);
+        });
+
+        const storage = getStorage();
+        const storageRef = ref(storage, `ProfilePictures/${auth.currentUser.uid}`);
+        const uploadTask = uploadBytesResumable(storageRef, blob);
+
+        uploadTask.on(
+            'state_changed',
+            (snapshot) => {
+                // Handle progress, such as updating UI elements to show progress
+                const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                console.log('Upload is ' + progress + '% done');
+                setUploading(true);
+            },
+            (error) => {
+                // Handle unsuccessful uploads
+                setUploading(false);
+                console.error('Error uploading:', error);
+                blob.close();
+            },
+            () => {
+                // Handle successful uploads on completion
+                uploadTask.snapshot.ref.getDownloadURL().then((downloadURL) => {
+                    setUploading(false);
+                    console.log('Download URL:', downloadURL);
+                    setProfileImage(downloadURL);
+                    blob.close();
+                });
+            }
+        );
+    };
+
     return (
         <View style={styles.container}>
+            <View style={styles.profilePictureContainer}>
+                <View style={styles.profileImageContainer}>
+                    {profileImage && <Image source={{ uri: profileImage }} style={styles.profileImage} />}
+                </View>
+                <TouchableOpacity style={styles.uploadImageButton} onPress={handleProfilePictureUpload}>
+                    <Text style={styles.uploadImageButtonText}>Upload Profile Picture</Text>
+                </TouchableOpacity>
+            </View>
+            <WhiteLine />
+
             <View style={styles.ContainerWrap}>
                 <Text style={styles.headerText}>Profile Details</Text>
 
@@ -79,9 +178,8 @@ const styles = StyleSheet.create({
         marginBottom: 20,
     },
     ContainerWrap: {
-       
         width: '80%',
-        height: '50%',
+        height: '60%', // Adjust the height to accommodate the profile picture
         justifyContent: 'space-around',
         alignItems: 'center',
     },
@@ -125,7 +223,7 @@ const styles = StyleSheet.create({
         color: 'black',
         textAlign: 'center',
         fontWeight: 'bold',
-        fontSize:18,
+        fontSize: 18,
     },
     logoutButton: {
         backgroundColor: 'red',
@@ -137,13 +235,43 @@ const styles = StyleSheet.create({
         color: '#fff',
         textAlign: 'center',
         fontWeight: 'bold',
-        fontSize:18,
+        fontSize: 18,
     },
     whiteLine: {
         backgroundColor: 'white',
         height: 2,
         width: '100%',
         marginVertical: 10,
+    },
+    profilePictureContainer: {
+        alignItems: 'center',
+        marginBottom: 20,
+    },
+    profileImageContainer: {
+        borderColor: 'white',// Add this line for the outer white border
+        borderWidth: 2,// Add this line for the outer white border
+        borderRadius: 50, // Set the radius to half of the width and height of the profile picture
+        width: 104,           // Set the width to the diameter + borderWidth * 2
+        height: 104,          // Set the height to the diameter + borderWidth * 2
+        padding: 4,// Adjust this value accordingly based on your needs
+    },
+    profileImage: {
+        width: 100,
+        height: 100,
+        borderRadius: 50,
+        marginBottom: 10,
+    },
+    uploadImageButton: {
+        backgroundColor: 'white',
+        padding: 10,
+        borderRadius: 5,
+        margin: 10,
+    },
+    uploadImageButtonText: {
+        color: 'black',
+        textAlign: 'center',
+        fontWeight: 'bold',
+        fontSize: 18,
     },
 });
 
